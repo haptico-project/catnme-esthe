2f0da39322adb564f6dc0bf4e266d5b9ef90105d
digiot-abc <digiot-abc@users.noreply.github.com>
2025-09-18 16:06:29 +0000

    deploy: f2e3b42f1e07792c72ffe19d87b60db839b31b90

---
+++ _app/immutable/nodes/2.BtdKv1zI.js
@@ -0,0 +1,15 @@
+import{S as n,i as o,s as c,n as s,d as b,a as d,b as m,c as v,g,e as h}from"../chunks/BEYSVC5Y.js";import"../chunks/IHki7fMi.js";import{b as r}from"../chunks/DdT90mFW.js";function u(i){let e,a=`<figure class="relative w-full h-[min(72vh,900px)] overflow-hidden"><img src="${`${r}/images/cat-main.png`}" alt="" class="absolute inset-0 w-full h-full object-cover"/> <div class="absolute top-3 right-3"><img src="${`${r}/images/logo.svg`}" alt="logo" class="h-8 w-auto"/></div></figure> <section class="relative" style="padding-top: 80px;"><div class="text-center">「猫とわたしのエステ」は、<br/>
+			熟練マッサージ師の手の動きを再現した振動器です。<br/>
+			やわらかな振動が、<br/>
+			心と体を静かにゆるめていきます。<br/> <br/>
+
+			そして、<br/>
+			この振動器のいちばんの魅力は<br/>
+			猫と一緒に使えること。<br/>
+			通常の振動モードにくわえて、<br/>
+			猫の小さな体にもやさしい、穏やかなモードが選べます。<br/> <br/>
+			猫がゴロゴロと喉を鳴らし、<br/>
+			私も目を閉じて、夢の中へ...<br/> <br/>
+			猫と一緒に、やさしく癒される。<br/>
+			そんな、日常に小さな贅沢を添える<br/>
+			新しいエステ体験をお楽しみください。<br/></div></section> <div class="h-10"></div>`;return{c(){e=h("section"),e.innerHTML=a,this.h()},l(t){e=v(t,"SECTION",{class:!0,"data-svelte-h":!0}),g(e)!=="svelte-1dicb2k"&&(e.innerHTML=a),this.h()},h(){m(e,"class","text-center relative")},m(t,l){d(t,e,l)},p:s,i:s,o:s,d(t){t&&b(e)}}}class _ extends n{constructor(e){super(),o(this,e,null,u,c,{})}}export{_ as component};
